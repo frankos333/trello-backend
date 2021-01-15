3abde96a9484f2c11e6234ee19ce43125f8c4d8f
Nadav_Frank <nadavf@misterbit.co.il>
2021-01-15 21:22:15 +0200

    fixed route function name to update

--- api/board/board.routes.js
+++ api/board/board.routes.js
@@ -1,10 +1,10 @@
 const express = require('express')
-const {get, remove, add, getById, edit} = require('./board.controller')
+const {get, remove, add, getById, update} = require('./board.controller')
 const router = express.Router()
 
 router.get('/', get)
 router.get('/:id', getById);
-router.put('/:id', edit);
+router.put('/:id', update);
 router.delete('/:id', remove)
 router.post('/', add);
 
--- config/index.js
+++ config/index.js
@@ -1,6 +1,7 @@
 let config;
 
-config = require('./prod')
+// config = require('./prod')
+config = require ('./dev')
 // if (process.env.NODE_ENV === 'production') config = require('./prod')
 // else config = require('./dev')
 
